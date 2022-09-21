565b7d9f463520b36559a4ee87ca103844c7e1ab
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-09-21 15:26:23 -0700

    added class names and props to buttons

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,17 +2,14 @@ import Button from "./utilities/Button"
 
 const Header = () => {
   return (
-    <header>
-        <nav>    
-            <div className="conatiner">
-                <div>
-                  <Button className={"main-btn"} text={"Main"} />
-                </div>
-                <div>
-                    {/* <Button />
-                    <Button /> */}
-                </div>
-                
+    <header> 
+        <nav className="container">
+            <div>
+              <Button className={"main-btn | fs-600 fw-bold"} text={"main"} />
+            </div>
+            <div>
+                <Button className={"projects-btn | fs-600 fw-bold"} text={"projects"}/>
+                <Button className={"contact-btn | fs-600 fw-bold margin-inline-start-36"} text={"contact"}/>
             </div>
         </nav>
     </header>
