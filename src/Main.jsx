09cd14a1ac5e8bdb4f12d97fbe69387a0b138a79
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-09-21 09:15:16 -0700

    set up base files for main components

---
+++ src/Main.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Hero from './hero/Hero'
+import Projects from './projects/Projects'
+import Contact from './contact/Contact'
+
+function Main() {
+
+
+  return (
+    <main className="main">
+      Main
+      <Hero />
+      <Projects />
+      <Contact />
+    </main>
+  )
+}
+
+export default Main
