00f437c04f779c69f517b026ce6eef9bcb63207c
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-09-22 14:55:36 -0700

    update to add smaller projects

--- src/projects/projectObjs.js
+++ src/projects/projectObjs.js
@@ -17,7 +17,7 @@ const allProjects = {
 			title: "BookShelf",
 			description:
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit!",
-			img: ["", ""],
+			img: ["src/assets/test-img.jpg", "src/assets/test-img-two.jpg"],
 			link: {
 				demo: "",
 				code: "",
@@ -26,10 +26,36 @@ const allProjects = {
 		},
 		{
 			id: 3,
+			title: "Personal Wedsite",
+			description:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit!",
+			img: ["src/assets/test-img.jpg", "src/assets/test-img-two.jpg"],
+			link: {
+				demo: "",
+				code: "",
+				projectPage: "",
+			},
+		},
+	],
+	smallProjects: [
+		{
+			id: 1,
+			title: "Portfolio Site",
+			description:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit!",
+			img: ["src/assets/test-img.jpg", "src/assets/test-img-two.jpg"],
+			link: {
+				demo: "",
+				code: "",
+				projectPage: "",
+			},
+		},
+		{
+			id: 2,
 			title: "BookShelf",
 			description:
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit!",
-			img: ["", ""],
+			img: ["src/assets/test-img.jpg", "src/assets/test-img-two.jpg"],
 			link: {
 				demo: "",
 				code: "",
