7e95a3b15c45156e645a6d170ef66ae166b7100d
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-09-27 08:35:34 -0700

    removed button.jsx and built out the buttons in each component

--- src/utilities/Button.jsx
+++
@@ -1,7 +0,0 @@
-const Button = (props) => {
-  return (
-    <button onClick={props.className === "toggle-nav-btn" ? props.toggleMenu : null } className={`btn ${props.className}`}>{props.text}</button>
-  )
-}
-
-export default Button
