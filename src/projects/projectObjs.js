41652d9e0cbc14c018f8440eaa86b634ab8f4170
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-09-28 07:31:07 -0700

    update to content on one of the small projects

--- src/projects/projectObjs.js
+++ src/projects/projectObjs.js
@@ -2,7 +2,7 @@ const allProjects = {
 	mainProjects: [
 		{
 			id: 1,
-			title: "Portfolio Site",
+			title: "Personal Website",
 			description:
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit!",
 			img: ["src/assets/test-img.jpg", "src/assets/test-img-two.jpg"],
@@ -26,7 +26,7 @@ const allProjects = {
 		},
 		{
 			id: 3,
-			title: "Personal Wedsite",
+			title: "Portfolio Site",
 			description:
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit!",
 			img: ["src/assets/test-img.jpg", "src/assets/test-img-two.jpg"],
@@ -40,21 +40,31 @@ const allProjects = {
 	smallProjects: [
 		{
 			id: 1,
-			title: "Portfolio Site",
-			description:
-				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit!",
-			img: ["src/assets/test-img.jpg", "src/assets/test-img-two.jpg"],
+			title: "Restaurant Website",
+			languages: ["JavaScrip", "CSS", "WebPack"],
+			description: [
+				"Project is an assignment from The Odin Project course.",
+				"Site uses vanilla JS to create the differant pages within the restaurant website, and controlls all user interations.",
+				"Design and layout was created by me, with styling done using base CSS.",
+				"Site is responsive to accommodate both desktop and mobile views",
+			],
+			img: [
+				"src/assets/restaurant-one.PNG",
+				"src/assets/restaurant-two.PNG",
+			],
 			link: {
-				demo: "",
-				code: "",
+				demo: "https://nighistheend.github.io/top-restaurant-page/",
+				code: "https://github.com/NighIsTheEnd/top-restaurant-page",
 				projectPage: "",
 			},
 		},
 		{
 			id: 2,
 			title: "BookShelf",
-			description:
+			languages: ["React", "JavaScrip"],
+			description: [
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia odio, tempora illum id, aliquid, labore quas similique suscipit vel nam ab quasi voluptatibus libero cum! Expedita, non? Tempora, impedit!",
+			],
 			img: ["src/assets/test-img.jpg", "src/assets/test-img-two.jpg"],
 			link: {
 				demo: "",
