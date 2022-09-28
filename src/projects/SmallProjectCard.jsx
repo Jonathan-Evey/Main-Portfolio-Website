c6aef6e804f13e5a304637dbb22b2aefac62b411
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-09-28 07:32:01 -0700

    small adjustment to how the cards generate based on minor updateds to data layout

--- src/projects/SmallProjectCard.jsx
+++ src/projects/SmallProjectCard.jsx
@@ -6,11 +6,16 @@ const SmallProjectCard = ({project}) => {
             <img src={project.img[1]} alt="" />
         </div>
         <div className="container-project-text | padding-inline-24">
-            <h4 className="title | clr-neutral-no-color-white fs-600 fw-bold margin-inline-end-32 ">{project.title}</h4>
-            <p className="fs-300 margin-block-start-20">{project.description}</p>
+            <h4 className="title | clr-neutral-no-color-white fw-bold margin-inline-end-32 ">{project.title}</h4>
+            <div className="flex margin-block-start-16">
+              {project.languages.map((each, index) => <p className="clr-neutral-no-color-white fs-400 fw-bold margin-inline-start-16 margin-block-end-20" key={index}>{each}</p>)}
+            </div>
+            <div>
+              {project.description.map((each, index) => <p className="fs-300 margin-block-end-8" key={index}>{each}</p>)}
+            </div>
             <div className="container-project-links">
-                <a className="link project" href="#">Demo</a>
-                <a className="link project" href="#">Code</a>
+                <a className="link project" href={project.link.demo}>Demo</a>
+                <a className="link project" href={project.link.code}>Code</a>
                 <a className="link project" href="#">Write Up</a>
             </div>
         </div>
