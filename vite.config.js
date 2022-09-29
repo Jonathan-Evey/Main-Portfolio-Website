68349457e5ad818b561da37a6b3d10eff9597207
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-09-29 06:07:50 -0700

    update to file path in dev mode

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/Main-Portfolio-Website/",
+	// base: "/Main-Portfolio-Website/",
 	plugins: [react()],
 });
