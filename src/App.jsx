3ef989243c9c5ea4aad06525a9feb0b068bef0df
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-09-29 06:07:12 -0700

    update to inclue react Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { Home } from "./pages/Home"
+import Home from "./pages/home/Home"
 import { Route, Routes } from 'react-router-dom';
+import ProjectRoutes from "./pages/ProjectRoutes";
 
 const App = () => {
+
     return (
-    <Routes>
-        <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+        
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/project-write-up/*" element={<ProjectRoutes />} />
+        </Routes>
+    </>
+    )
 }
 
 export default App
