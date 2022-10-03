3118759dcb792ffb9260b6037115962f3adf73f9
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-10-03 14:27:29 -0700

    update to synce buttons better

--- src/intersectionObserver.js
+++ src/intersectionObserver.js
@@ -1,11 +1,15 @@
 const setUpObserver = () => {
 	let eachSection = document.querySelectorAll(".section");
+
 	const mainObserver = new IntersectionObserver(
 		(entries) => {
 			entries.forEach((entry) => {
 				document
 					.querySelector(".main-btn")
 					.classList.toggle("show", !entry.isIntersecting);
+				document
+					.querySelector(".projects-btn")
+					.classList.toggle("show", entry.isIntersecting);
 			});
 		},
 		{
@@ -13,31 +17,33 @@ const setUpObserver = () => {
 		}
 	);
 	mainObserver.observe(eachSection[0]);
-
-	const projectsObserver = new IntersectionObserver(
-		(entries) => {
-			entries.forEach((entry) => {
-				document
-					.querySelector(".projects-btn")
-					.classList.toggle("show", !entry.isIntersecting);
-			});
-		},
-		{
-			rootMargin: "35% 0% 0% 0%",
-			threshold: 0.25,
-		}
-	);
-	projectsObserver.observe(eachSection[1]);
+	// const projectsObserver = new IntersectionObserver(
+	// 	(entries) => {
+	// 		entries.forEach((entry) => {
+	// 			console.log("enter");
+	// 			document
+	// 				.querySelector(".projects-btn")
+	// 				.classList.toggle("show", !entry.isIntersecting);
+	// 		});
+	// 	},
+	// 	{
+	// 		threshold: 0.15,
+	// 	}
+	// );
+	// projectsObserver.observe(eachSection[1]);
 	const contactObserver = new IntersectionObserver(
 		(entries) => {
 			entries.forEach((entry) => {
 				document
 					.querySelector(".contact-btn")
 					.classList.toggle("show", !entry.isIntersecting);
+				document
+					.querySelector(".projects-btn")
+					.classList.toggle("show", entry.isIntersecting);
 			});
 		},
 		{
-			threshold: 0.6,
+			threshold: 0.65,
 		}
 	);
 	contactObserver.observe(eachSection[2]);
