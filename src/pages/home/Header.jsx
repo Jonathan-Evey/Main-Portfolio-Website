1882dc66e910eb747a1624921592b5a058649dec
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-10-03 14:27:55 -0700

    update to synce buttons better

--- src/pages/home/Header.jsx
+++ src/pages/home/Header.jsx
@@ -48,8 +48,8 @@ const location = useLocation();
               <button onClick={scrollToHero} className="btn main-btn nav-btn underline | fs-600 fw-bold">main</button>
             </div>
             <div>
-                <button onClick={scrollToProjects} className="btn projects-btn nav-btn underline show | fs-600 fw-bold">projects</button>
-                <button onClick={scrollToContact} className="btn contact-btn nav-btn underline show | fs-600 fw-bold margin-inline-start-36">contact</button>
+                <button onClick={scrollToProjects} className="btn projects-btn nav-btn underline | fs-600 fw-bold">projects</button>
+                <button onClick={scrollToContact} className="btn contact-btn nav-btn underline | fs-600 fw-bold margin-inline-start-36">contact</button>
             </div>
         </nav>
         <button onClick={toggleMenu} className="btn toggle-nav-btn"></button>
