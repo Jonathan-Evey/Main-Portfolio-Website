382801470fe74ac9f0a56e1c3799c0e3869f1aec
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-10-03 14:30:25 -0700

    update to add padding at the bottom of layout

--- src/pages/home/projects/Projects.jsx
+++ src/pages/home/projects/Projects.jsx
@@ -4,7 +4,7 @@ import SmallProjectsContainer from "./SmallProjectsContainer"
 
 const Projects = forwardRef((props, ref) => {
   return (
-    <section className="section" id='projects' ref={ref}>
+    <section className="section | padding-block-end-144" id='projects' ref={ref}>
       <div className="container">
         <h2 className="title projects | fs-900 fw-bold margin-block-end-96">Projects</h2>
         <MainProjectsContainer />
