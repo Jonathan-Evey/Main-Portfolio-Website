711151715ce46ad9644766f406b84ff0fab337b4
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-10-05 10:10:16 -0700

    working on filling in the site content

--- src/pages/home/contact/Contact.jsx
+++ src/pages/home/contact/Contact.jsx
@@ -6,12 +6,12 @@ const Contact = forwardRef((props, ref) => {
     <section id='contact' className="section contact | magin-block-end-48" ref={ref}>
       <div className="container contact ">
         <h2 className="title | fs-900 fw-bold margin-block-end-56">Contact</h2>
-        <p className="margin-block-end-56">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusamus eos quod labore tempore error placeat ipsam voluptates dolore praesentium optio architecto, id molestias distinctio. Quo consequatur cupiditate ratione dignissimos.</p>
+        <p className="fs-500 margin-block-end-56">Have any questions, comments, or looking to work with me? Feel free to send me a message, or reach out to me on my <a href='#socials' className='link project'>socials</a>. I look forward to hearing from you.</p>
       <ContactForm />
       </div>
 
       <div className="container find-me">
-        <h3 className="title | fs-900 fw-bold ">Find Me</h3>
+        <h3 id="socials" className="title | fs-900 fw-bold ">Find Me</h3>
         <a className="link social | fs-600 margin-block-start-80" href="https://github.com/NighIsTheEnd">gitHub</a>
         <a className="link social | fs-600 margin-block-start-80" href="http://">LinkedIn</a>
       </div>
--- src/pages/home/projects/projectObjs.js
+++ src/pages/home/projects/projectObjs.js
@@ -21,10 +21,13 @@ const allProjects = {
 			URL: "bookshelf",
 			languages: ["React", "CSS", "Axios"],
 			description: [
-				"Personal project where a user can find and save books to a digital bookshelf. The books have a updatable option for if the book is unread, being read, or read.",
-				"Site is built with React, and utilized the Axios library.",
-				"Book infromation is pull using Google Books API",
-				"Design and layout created using CSS. Project design is responsive to accommodate both desktop and mobile views",
+				"Personal project",
+				//"where a user can find and save books to a digital bookshelf. The books have a updatable option for if the book is unread, being read, or read.",
+				"Application utility allows a user to save books in a digital book-shelf. Once added, the user can update the book to show if they have read, are reading, or have read the book. If book is marked as read, user can rate the book",
+				"Technologies employed in the application are React, with Axios library, Google Books API and CSS",
+				//"Site is built with React, and utilized the Axios library.",
+				//"Book infromation is pull using Google Books API",
+				//"Design and layout created using CSS. Project design is responsive to accommodate both desktop and mobile views",
 			],
 			img: ["/src/assets/test-img.jpg", "/src/assets/test-img-two.jpg"],
 			link: {
@@ -39,11 +42,13 @@ const allProjects = {
 			URL: "personal-website",
 			languages: ["React", "SASS", "Figma", "Firebase"],
 			description: [
-				"Personal project designed to showcase current projects.",
-				"Site is built with React, utilizing React Router for page URL updates, and Vite for the development process.",
-				"Design and mock-up by self creacted within Figma.",
-				"Site layout was created using SASS, and is responsive to accommodate both desktop and mobile views",
-				"Deployed with Firebase",
+				"Personal project",
+				"Project utilization is to easily showcase completed projects, displaying current skills and abilities, and providing a convenient source to get in contact.",
+				// "Site is built with React, utilizing React Router for page URL updates, and Vite for the development process.",
+				// "Design and mock-up by self creacted within Figma.",
+				"Main resources used in project React/JavaScript to handle interactions with SASS for site styling.",
+				//"Site layout was created using SASS, and is responsive to accommodate both desktop and mobile views",
+				"Deployed with Firebase.",
 			],
 			img: ["/src/assets/test-img.jpg", "/src/assets/test-img-two.jpg"],
 			link: {
@@ -61,9 +66,9 @@ const allProjects = {
 			languages: ["JavaScript", "CSS", "WebPack"],
 			description: [
 				"Project is an assignment from The Odin Project course.",
-				"Concept is to creat and save Projects, with a title, description, completion due date, and a priority rating. On each project user can add Tasks that can be maked complete, or not",
+				"Assignment was to creat and save projects, with a title, description, completion due date, and a priority rating. On each project user can add tasks that can be maked complete, or not.",
 				"Functionality is controlled with JavaScript.  Design and layout created with CSS.",
-				"Project utilizes local storage to save user data",
+				"Project utilizes local storage to save user data.",
 			],
 			img: [
 				"/src/assets/project-manager-six.PNG",
@@ -85,10 +90,10 @@ const allProjects = {
 			URL: "restaurant website",
 			languages: ["JavaScript", "CSS", "WebPack"],
 			description: [
-				"Project was an assignment from The Odin Project course.",
-				"Concept is to built a site using JavaScript to render DOMelements. Once a  user selects a new page from the main navigation, new page will render in without linking to a new html index.",
+				"Project is an assignment from The Odin Project course.",
+				"Assignment was to built a site using JavaScript to render DOMelements. Once a user selects a new page from the main navigation, new page will render in without linking to a new html index.",
 				"Site is built with JavaScript. Design and styling done using CSS.",
-				"Site is responsive to accommodate both desktop and mobile views",
+				"Site is responsive to accommodate both desktop and mobile views.",
 			],
 			img: [
 				"/src/assets/restaurant-one.PNG",
