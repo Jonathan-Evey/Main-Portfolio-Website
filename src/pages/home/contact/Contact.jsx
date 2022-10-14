a81149d516afd9af490e434a6304b9aca633ceab
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-10-14 08:45:08 -0700

    update to hero animation, minor style updates

--- src/pages/home/contact/Contact.jsx
+++ src/pages/home/contact/Contact.jsx
@@ -2,10 +2,15 @@ import React, { forwardRef, useRef } from 'react'
 import ContactForm from "./ContactForm"
 
 const Contact = forwardRef((props, ref) => {
+  let rings = [];
+
+  for (let i = 0; i <= 5; i++) {
+    rings.push(i);
+  }
   return (
     <section id='contact' className="section contact | magin-block-end-48" ref={ref}>
       <div className="container contact ">
-        <h2 className="title | fs-900 fw-bold margin-block-end-56">Contact</h2>
+        <h2 className="title | fs-900 fw-bold margin-block-end-80">Contact</h2>
         <p className="fs-500 margin-block-end-56">Have any questions, comments, or looking to work with me? Feel free to send me a message, or reach out to me on my <a href='#socials' className='link project'>socials</a>. I look forward to hearing from you.</p>
       <ContactForm />
       </div>
@@ -15,7 +20,9 @@ const Contact = forwardRef((props, ref) => {
         <a className="link social | fs-600 margin-block-start-80" href="https://github.com/NighIsTheEnd">gitHub</a>
         <a className="link social | fs-600 margin-block-start-80" href="http://">LinkedIn</a>
       </div>
-      <div className="bg-large-sphere contact"></div>
+      <div className="bg-large-sphere contact">
+        {rings.map((ring, index) => (<div style={{ "--i": `${index}`}} className="ring" key={ring}></div>))}
+      </div>
     </section>
   )
 });
--- src/pages/home/projects/Projects.jsx
+++ src/pages/home/projects/Projects.jsx
@@ -4,7 +4,7 @@ import SmallProjectsContainer from "./SmallProjectsContainer"
 
 const Projects = forwardRef((props, ref) => {
   return (
-    <section className="section | padding-block-end-144" id='projects' ref={ref}>
+    <section className="section projects | padding-block-end-144" id='projects' ref={ref}>
       <div className="container">
         <h2 className="title projects | fs-900 fw-bold margin-block-end-144">Projects</h2>
         <MainProjectsContainer />
