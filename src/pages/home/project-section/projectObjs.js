1c03b47c3c55ebac908a609da55fa58fdeb85f10
Jonathan Evey <98228317+Jonathan-Evey@users.noreply.github.com>
2022-10-24 09:33:38 -0700

    update to project details and descriptions

--- src/pages/home/project-section/projectObjs.js
+++ src/pages/home/project-section/projectObjs.js
@@ -6,9 +6,9 @@ const allProjects = {
 			URL: "personal-website",
 			languages: ["React", "SASS", "Figma", "Firebase"],
 			description: [
-				"This is my personal portfolio site made to showcase my completed projects, displaying current skills and abilities, and providing a convenient source to get in contact with me.",
-				"Site is a static website, but was made using React to reinforce my knowledge of React. This gave me the opportunity to work with React Router, and Formik for the first time.",
-				"Main resources used in the project are React/JavaScript, Figma was used for design, with SASS for site styling. Site was deployed using Firebase.",
+				"This is my personal website made to showcase my current skills and abilities by displaying projects I have completed.",
+				"Project is a static website made using React to give me the opportunity to work with React Router to handle page redirection, and Formik to handle the contact form interactions.",
+				"Technologies utilized in this project are Figma, used to create the design, React/Javascript with SASS for site layout, styling and functions, with Firebase used to deploy and host.",
 			],
 			img: [
 				"/assets/personal-website-five.PNG",
@@ -20,14 +20,14 @@ const allProjects = {
 			],
 			wrightUp: {
 				projectSummary:
-					"Personal website to display projects that I have completed, and a convenient resource for someone to get in contact with me.",
+					"My personal website was made to be a convenient place to showcase my abilities by displaying the projects that I have completed, and a convenient resource for someone to get in contact with me.",
 				overview:
-					"I made the Website using React and SASS as the main technologies. Site design, layout and mock-ups were self created by me using Figma, with SASS used for the site styling. I utilized React Router to render each project page once a project’s overview link is selected.",
+					"Website was made using React as a way to get additional experience with the React framework. This opened up the opportunity to work with React Router for handling page redirection, and Formlik to handle the contact form validation, and form state. I designed the site using Figma to create the design and mock-ups, and I used SASS for the layout and styling of the website. Site was deployed and hosted using Fireship and uses Firestore to store the messages submitted from the contact form.",
 				mainChallenges: [
-					"First application that I have completed that has been fully deployed. First application that I have worked with SASS for styling, with a more scalable approach in the CSS structure and class naming.",
+					"First website that I have fully deployed, and set up a database that interacts with a website, through Fireship. I used SASS for the first time in this project, and worked on making my SASS/CSS more scalable by using reusable classes that handle one styling change, and using more individual class names when a larger amount of styling is needed.",
 				],
 				commentsTakeaways:
-					"This website did not need to be built with React, but I chose to do so to reinforce my knowledge of React. This allowed me to make a React contact form, and gave me exposure to React Router.",
+					"Website is a static site and did not need to be built with React, but I chose to do so to reinforce my knowledge of React.",
 				completionDate: "October, 2022",
 			},
 			link: {
@@ -41,8 +41,8 @@ const allProjects = {
 			URL: "bookshelf",
 			languages: ["React", "CSS", "Axios"],
 			description: [
-				"This was a personal project that I made to reinforce my working knowledge of React.",
-				"Project is an application where a user can save books to a digital book-shelf. Once added, the user can update each book to show their read status of the book. Users can also add notes, and if the book is marked as read, users can add a review and rating.",
+				"This is a personal project that I made to reinforce my working knowledge of React.",
+				"Project is an application where a user can save books to a digital book-shelf. Main features include, searching the bookshelf by title or author, sorting shelf by author, title or rating, filtering the shelf based on read status, and or rating, and adding or viewing notes and reviews on each book.",
 				"Technologies I employed in the application are React, with Axios library, Google Books API and CSS.",
 			],
 			img: [
@@ -55,14 +55,14 @@ const allProjects = {
 			],
 			wrightUp: {
 				projectSummary:
-					"BookShelf is a personal project where a user can find and save books to a digital book-shelf. Once saved to the shelf, users can update each book to include notes, a review, a rating, and the current read status of each book. Project is a single-page application using React to generate, and update the content of the application.",
+					"BookShelf is a single-page application where a user can find and save books to a digital book-shelf. Once saved to the shelf, users can update each book in the following ways: add, edit, and delete notes, add, edit or delete a review, add and update a rating, and show the current read status of each book unread, reading and read.",
 				overview:
-					"Project concept was to incorporate data pulled from an API into an application that lets users add, update and change the data once it has been added and saved by the user. Main functions currently include, searching the shelf for a book by title or author, sorting the books on the shelf by author, title or rating, and filtering the shelf based on read status, and or rating. Application is built with the base React JavaScript, with Axios library for API interactions. All menus, dropdowns and functions are custom built for application, with CSS being used for project styling.",
+					"Project concept was to incorporate data pulled from an API into an application that lets users add, update and change the data once it has been added and saved by the user. Application is built with React JavaScript, and uses the Axios library for API interactions. All menus, dropdowns and functions are custom built for application, with CSS being used for project styling.",
 				mainChallenges: [
-					"This project is the first application that I utilized React, using State and Effect to make the application responsive. All menus and drop-down components have been made custom, in place of using a library, to better reinforce my knowledge of React. I used Google books API to pull the data for each book displayed.",
+					"This project is the first application that I made utilizing React, and using State and Effect to make the application responsive based on the current state of the book objects. Menu and drop-down components were all made custom, in place of using a library, to better reinforce my knowledge of React.",
 				],
 				commentsTakeaways:
-					"I did not pre-plan the full functionality of this project, or outline a design before starting work on the project. This leads to each update in features causing a layout and style update. Without a base plan of the project, it has made the scaling of the project more difficult and time consuming, with a lot of time still needed to refactor the applications CSS file.",
+					"I did not spend the time to pre-plan this project, or outline a design, before starting to work on the functionality of the project. By not pre-planning the project, it has made the scaling of the project more difficult and time consuming, and has led to each update in features causing extra time being spent to update the layout and style. There is a lot of time still needed to refactor the applications CSS file and the project still needs to have user authentication added to allow users to log in and save their data.",
 				completionDate: "October, 2022",
 			},
 			link: {
@@ -78,9 +78,9 @@ const allProjects = {
 			URL: "project-manager",
 			languages: ["JavaScript", "CSS", "WebPack"],
 			description: [
-				"This was an assignment from The Odin Project used to reinforce knowledge of JS modules and DOM manipulation.",
-				"Assignment was to be able to create and save a Todo list. Each list needed to have a title, description, completion due date, and a priority rating. On each list users can add tasks that can be marked complete, or not.",
-				"Functionality is controlled with JavaScript, utilizing local storage to save user data. Design and layout created with CSS.",
+				"This project was an assignment from The Odin Project course used to reinforce knowledge of JavaScript modules and DOM manipulation.",
+				"Assignment was to make an application where a user can create and save todo lists.  On each list users can add tasks that can be shown as completed, or not. Each list needed to have a title, description, completion due date, and a priority rating.",
+				"Functionality is controlled with JavaScript, utilizing local storage to save user data. Design and layout was created with CSS.",
 			],
 			img: [
 				"/assets/project-manager-six.PNG",
@@ -92,14 +92,14 @@ const allProjects = {
 			],
 			wrightUp: {
 				projectSummary:
-					"This project is the Todo List project from The Odin Project - JavaScript Course. Project application is to store and display Todo lists for a user. Each list has a title, description, completion due date and priority. Each list contains a sub-checklist, that you are able to add, edit, make complete or delete.The project is a single-page application that uses JavaScript to generate, and update the content of the application.",
+					"This project is the Todo List project from The Odin Project - JavaScript Course. Project application is to store and display todo lists added by the user. Each list has a title, description, completion due date and a priority. Each list contains a sub-checklist, that you are able to add, edit, make complete or delete.",
 				overview:
-					"In place of a Todo list, I updated the application idea to a project manager dashboard. Users can add the project name, description, completion due date, and a project urgency. Once added to the dashboard, users can click on the project to open a full view of that project. From the full view users can add tasks for the project, mark tasks completed, and delete tasks. The project list is sortable by tasks on each project, project urgency, and by completion date. Users can edit the project details, and delete each project. When viewing the tasks, tasks can be filtered by all, open, and completed.",
+					"This project is a single-page application that uses JavaScript to generate, and update the content of the application, with CSS used for styling. In place of a todo list, I updated the application idea to a project manager dashboard. Users can add a project with a name, description, completion due date, and an urgency level. Once added to the dashboard, users can click on the project to open a full view of that project. From the full view, users can add tasks for each project, mark tasks as completed, and delete tasks. The project list is sortable by total tasks on each project, project urgency, and by completion date. Users can edit the project details, and delete each project. When viewing the tasks on each project, tasks can be filtered by all, open, and completed.",
 				mainChallenges: [
-					"Project was the first application I made that lets a user add, and update data that gets saved in local storage. That same data is used to generate the display for the user making heavy use of DOM manipulation.",
+					"This project uses objects to store the data that is added by the user and was the first application I made that lets a user add, and update data. JavaScript was used to create the HTML elements that display the data added by the user. Application makes heavy use of DOM manipulation to update what is currently being displayed to the user.",
 				],
 				commentsTakeaways:
-					"Currently is not fully responsive, and will not display properly on small mobile screens. The layout for sorting the projects is a bit confusing and could use a redesign if the application was fully deployed. Currently, the add project and add task pop up windows do not have a close button, and can only be closed with the esc key. When deleting a project or task there is not a confirmation for the user, leaving a higher chance for an accidental deletion. Tasks do not have an option to be updated.",
+					"Currently the project is not fully responsive, and will not display properly on small mobile screens. The layout for sorting the projects is a bit confusing and could use a redesign if the application was to be fully deployed. Currently, the add project and add task pop up windows do not have a close button, and can only be closed with the esc key, and when deleting a project or task there is no confirmation for the user, leaving a higher chance for an accidental deletion. Tasks do not have an option to be updated.",
 
 				completionDate:
 					"July, 2022. Additional functionality updates October, 2022",
@@ -115,20 +115,20 @@ const allProjects = {
 			URL: "restaurant website",
 			languages: ["JavaScript", "CSS", "WebPack"],
 			description: [
-				"This was an assignment from The Odin Project used to reinforce knowledge of JS modules and DOM manipulation.",
-				"Assignment was to build a single page website using JavaScript to re-render DOM-elements. Site requirement was to have multiple pages, without each page having its own HTML index, that renders once a user selects that page from the main navigation.",
-				"Website is built using JavaScript, and design and styling was created with CSS. Site was made responsive to accommodate both desktop and mobile views.",
+				"This project was an assignment from The Odin Project used to reinforce knowledge of JavaScript modules and DOM manipulation.",
+				"Assignment requirement was to build a multi-page website using only one HTML file and use JavaScript to re-render the DOM-elements once a user selects a new page from the main navigation.",
+				"Website is built using JavaScript, with CSS used for the design and stylin. Site was made responsive to accommodate both desktop and mobile views.",
 			],
 			wrightUp: {
 				projectSummary:
-					"The Restaurant Page project is from The Odin Project - JavaScript Course. Project is a multi-page website that uses JavaScript to generate the content of the website instead of a different HTML file for each page.",
+					"The Restaurant Page project is from The Odin Project - JavaScript Course. Project is a multi-page website that uses JavaScript to generate each page of the website instead of a different HTML file for each page.",
 				overview:
-					"I approached this project by making a mockup of the site with just HTML and CSS, and then built out the functionality of the site with JavaScript. At this time I used the HTML files from the mockups, and made each file a template and stored them in a JavaScript module. Once the corresponding page is clicked from the main navigation, the template is inserted after the body element instead of generating each HTML element individually with JavaScript.",
+					"I approached this project by making a mockup of the site with just HTML and CSS, and then built out the functionality of the site with JavaScript. At the time I made each HTML page  mockup into a template and stored them in a JavaScript module. Once the corresponding page is clicked from the main navigation, the template is inserted after the body element instead of generating each HTML element individually with JavaScript.",
 				mainChallenges: [
-					"This is the first project I made using WebPack, and JavaScript ES6 modules, to bundle a project's JavaScript files together, giving me the opportunity to learn how to import and export JS files or functions into a different file. I made use of CSS media queries, flex and grid to make the site responsive for mobile view. This was not part of the project's outline, but in a real use case for a restaurant site it would need to be responsive.",
+					"This is the first project I made using WebPack, and JavaScript ES6 modules, to bundle a project's JavaScript files together, giving me the opportunity to learn how to import and export JavaScript files and functions into other files. I made use of CSS media queries, flex and grid to make the site responsive for mobile view. This was not part of the project's outline, but in a real use case for a restaurant site it would need to be responsive.",
 				],
 				commentsTakeaways:
-					"CSS makes heavy use of individual element class names for styling making it more difficult to scale and update.",
+					"CSS makes heavy use of individual element class names for styling making it more difficult to scale and update if needed.",
 				completionDate: "June, 2022. Minor style updates October, 2022",
 			},
 
