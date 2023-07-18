2bdc726f71f08e176ee99872837f887fa68cd45a
Jonathan-Evey <jonathan.c.evey@gmail.com>
2023-07-18 11:52:04 -0700

    update to text on main page

--- src/pages/home/hero/Hero.jsx
+++ src/pages/home/hero/Hero.jsx
@@ -14,7 +14,8 @@ const Hero = ({heroRef}) => {
       <div className="hero container">
         <h1 className="title | fs-900 fw-bold">Jonathan</h1>
         <h1 className="title | fs-900 fw-bold margin-block-end-40">Evey</h1>
-        <p className="fs-500 margin-block-32">Hello and welcome! I am a self taught developer that started out in front end development and am working towards becoming proficient as a full stack developer. My main skill set currently includes JavaScript, React, CSS and SASS.</p>
+        <p className="fs-500 margin-block-32">Hello and welcome! I am an aspiring developer who embarked on a self-taught journey, starting with front-end development and now aiming to become proficient in full-stack development. Through my dedicated efforts, I have acquired a solid foundation in various technologies and tools that are essential for building modern web applications.</p>
+        <p className="fs-500">While front-end development has been my initial focus, I have been expanding my skills to encompass the entire stack. I am enthusiastic about learning back-end technologies, such as Node.js, with express.js, and databases like MongoDB, as they will enable me to create robust and scalable web applications.</p>
       </div>
     </section>
   )
